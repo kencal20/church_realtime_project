e136071c0b9469be48f6c82410b7304e336670ff
kencal <kenadjei20@gmail.com>
2023-04-24 18:35:42 +0000

    footer update

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,6 +3,8 @@ import { Col, Row } from "react-bootstrap";
 import "./footer.css";
 import logo4 from "../img/logo6.avif";
 export default function Footer() {
+  const Year = new Date().getFullYear();
+
   return (
     <div>
       <footer id="footer">
@@ -12,24 +14,50 @@ export default function Footer() {
               <h5>
                 <b>CONTACT US</b>{" "}
               </h5>
-              <p><i>Email</i></p>
-              <p><i>Phone</i></p>
-              <p><i>Whatsapp</i></p>
-              <a href="https://goo.gl/maps/Ti63XJ9B9iuo7wnM8"><i>location</i></a>
+              <p>
+                <i>Email</i>
+              </p>
+              <p>
+                <i>Phone</i>
+              </p>
+              <p>
+                <i>Whatsapp</i>
+              </p>
+              <a href="https://goo.gl/maps/Ti63XJ9B9iuo7wnM8">
+                <i>location</i>
+              </a>
             </Col>{" "}
             <Col>
-              <h5><b>DEPARTMENTS</b></h5>
-              <p><i>Music</i></p>
-              <p><i>Personal Ministries</i></p>
-              <p><i>Youth</i></p>
-              <p><i>Communication</i></p>
-          
+              <h5>
+                <b>DEPARTMENTS</b>
+              </h5>
+              <p>
+                <i>Music</i>
+              </p>
+              <p>
+                <i>Personal Ministries</i>
+              </p>
+              <p>
+                <i>Youth</i>
+              </p>
+              <p>
+                <i>Communication</i>
+              </p>
             </Col>
             <Col>
-              <h5><b>Support</b></h5>
-              <p><i>Mobile Money</i></p>
-              <p><i>Merchant Number</i></p>
-              <p><i>Account Number</i></p>
+              <h5>
+                <b>Support</b>
+              </h5>
+              <p>
+                <i>Mobile Money</i>
+              </p>
+              <p>
+                <i>Merchant Number</i>
+              </p>
+              <p>
+                <i>Account Number</i>
+              </p>
+              <p>All rights  reserved,&copy; {Year}</p>
             </Col>{" "}
           </Row>
         </div>{" "}
