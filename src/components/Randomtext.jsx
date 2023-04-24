cc81abcbc0f5237d9c9353619df080101abe909c
kencal <kenadjei20@gmail.com>
2023-04-24 12:32:08 +0000

    form randomText and courosel

---
+++ src/components/Randomtext.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+export default function Randomtext() {
+  return (
+    <div>
+      <br />
+      <br />
+      <h3> RandomTEXT</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+        commodo nunc libero. Vestibulum mattis imperdiet lacus. Morbi ac tempus
+        sem. Fusce quis libero arcu. Class aptent taciti sociosqu ad litora
+        torquent per conubia nostra, per inceptos himenaeos. Fusce id metus non
+        risus imperdiet facilisis ac nec tellus. Cras in fringilla massa. In ac
+        lectus eleifend, pharetra diam nec, posuere lorem. Pellentesque maximus
+        pretium volutpat. Nam quis laoreet ex. Donec quis leo lacus. Aliquam
+        faucibus mauris vitae tempor ullamcorper. Morbi commodo tellus id ligula
+        dapibus tempor. Mauris mollis elit urna, id vulputate turpis tempus sit
+        amet. Pellentesque consequat blandit sem, nec pellentesque neque laoreet
+        eu. Pellentesque tristique urna id orci mollis molestie. Nullam
+        eleifend, diam vel lobortis ultricies, mauris velit ornare sapien, a
+        tincidunt augue lacus vitae diam. Aenean mollis ante quis eros placerat,
+        eu pretium tortor fringilla. Aenean posuere tincidunt turpis tincidunt
+        malesuada. Nunc tempor odio vel nunc pretium tincidunt. Suspendisse
+        potenti. Pellentesque viverra felis nisl, sed pharetra felis dignissim
+        eget. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+        posuere cubilia curae; Nunc lacinia non nisl placerat tempus. Aliquam
+        sit amet dolor lorem. Ut sed facilisis lacus.
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
+        commodo nunc libero. Vestibulum mattis imperdiet lacus. Morbi ac tempus
+        sem. Fusce quis libero arcu. Class aptent taciti sociosqu ad litora
+        torquent per conubia nostra, per inceptos himenaeos. Fusce id metus non
+        risus imperdiet facilisis ac nec tellus. Cras in fringilla massa. In ac
+        lectus eleifend, pharetra diam nec, posuere lorem. Pellentesque maximus
+        pretium volutpat. Nam quis laoreet ex. Donec quis leo lacus. Aliquam
+        faucibus mauris vitae tempor ullamcorper. Morbi commodo tellus id ligula
+        dapibus tempor. Mauris mollis elit urna, id vulputate turpis tempus sit
+        amet. Pellentesque consequat blandit sem, nec pellentesque neque laoreet
+        eu. Pellentesque tristique urna id orci mollis molestie. Nullam
+        eleifend, diam vel lobortis ultricies, mauris velit ornare sapien, a
+        tincidunt augue lacus vitae diam. Aenean mollis ante quis eros placerat,
+        eu pretium tortor fringilla. Aenean posuere tincidunt turpis tincidunt
+        malesuada. Nunc tempor odio vel nunc pretium tincidunt. Suspendisse
+        potenti. Pellentesque viverra felis nisl, sed pharetra felis dignissim
+        eget. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices
+        posuere cubilia curae; Nunc lacinia non nisl placerat tempus. Aliquam
+        sit amet dolor lorem. Ut sed facilisis lacus.
+      </p>
+    </div>
+  );
+}
