a4dfee686cf7d33ec28d89c8b735cb64985c228e
kencal <kenadjei20@gmail.com>
2023-04-24 19:12:55 +0000

    map component update

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -4,6 +4,7 @@ export default function Map() {
   return (
     <div>
       <iframe
+      title="map"
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3970.969422290014!2d-0.14237872606700183!3d5.57153903349544!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xfdf855aa5f0c627%3A0x668794f16b7724cd!2sLa-South%20SDA%20Church!5e0!3m2!1sen!2sgh!4v1682357911486!5m2!1sen!2sgh"
         style={{
           width: "800",
