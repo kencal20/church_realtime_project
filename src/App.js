8602bc907189ec3e699b16c600ab5d69ef562435
kencal <kenadjei20@gmail.com>
2023-04-26 16:49:53 +0000

    publishing testing done

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ export default function App() {
           <NavBar />
           <UncontrolledExample />
           <Randomtext />
-          {/* <Map /> */}
+          <Map />
           {/* <Randomtext /> */}
           {/* <Forms />  */}
           <Footer />
